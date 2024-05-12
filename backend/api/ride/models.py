44ce58c51273fb408f48a4b51a191ee3e8a26669
yashrastogi543 <21bcs249@iiitdmj.ac.in>
2024-05-12 14:30:39 +0530

    some changes

--- backend/api/ride/models.py
+++ backend/api/ride/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
+
 class Ride(models.Model):
-      user_initiator=models.CharField(max_length=100)
-      src_location=models.CharField(max_length=100)
-      dst_location=models.CharField(max_length=100)
-      total_fare=models.IntegerField()
-      num_passengers=models.IntegerField()
-      start_datetime=models.DateTimeField()
-      end_datetime=models.DateTimeField()
+    user_initiator = models.CharField(max_length=100)
+    src_location = models.CharField(max_length=100)
+    dst_location = models.CharField(max_length=100)
+    total_fare = models.IntegerField()
+    num_passengers = models.IntegerField()
+    start_datetime = models.DateTimeField()
