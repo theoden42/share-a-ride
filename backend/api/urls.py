c7d4f91947a9d8cd3c11a4896ae3d0ab6fbdebb3
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2024-05-12 19:58:02 +0530

    fix formatting for api/urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin 
-from django.urls import path 
-from django.urls.conf import include 
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
-        path("admin/", admin.site.urls), 
-        # ride model urls to be added below 
-        # path("ride/", include(api.ride.urls)), 
-        ]
+    path("admin/", admin.site.urls),
+    # ride model urls to be added below
+    # path("ride/", include(api.ride.urls)),
+]
