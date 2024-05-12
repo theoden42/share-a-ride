b2686f8a0328a803e858ce9a4f702b3e064130cc
Prakash <dprakash2103@gmail.com>
2024-05-12 22:03:23 +0530

    final change

--- backend/api/user/models.py
+++ backend/api/user/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), primary_key = True ,on_delete= models.CASCADE)
-    PhoneNumber = PhoneNumberField(null = False, blank = True, unique = True)
+    phone_number = PhoneNumberField(null = False, blank = True, unique = True)
     
