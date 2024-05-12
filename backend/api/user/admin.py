08e44144979caf08b35df4109868994487706fba
Prakash <dprakash2103@gmail.com>
2024-05-12 22:15:44 +0530

    idendation

--- backend/api/user/admin.py
+++ backend/api/user/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin 
+from django.contrib import admin
 from api.user.models import Profile
 
 admin.site.register(Profile)
--- backend/api/user/models.py
+++ backend/api/user/models.py
@@ -2,7 +2,8 @@ from django.contrib.auth import get_user_model
 from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
 
+
 class Profile(models.Model):
-    user = models.OneToOneField(get_user_model(), primary_key = True ,on_delete= models.CASCADE)
-    phone_number = PhoneNumberField(null = False, blank = True, unique = True)
-    
+    user = models.OneToOneField(
+        get_user_model(), primary_key=True, on_delete=models.CASCADE)
+    phone_number = PhoneNumberField(null=False, blank=True, unique=True)
