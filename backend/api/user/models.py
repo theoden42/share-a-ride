ef900a17114f0eec738e75d661a8da3ca4bcd554
Prakash <dprakash2103@gmail.com>
2024-05-12 21:17:02 +0530

    user-model and maigrations of ride-model

---
+++ backend/api/user/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth import get_user_model
+from phonenumber_field.modelfields import PhoneNumberField
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), primary_key = True ,on_delete= models.CASCADE)
+    PhoneNumber = PhoneNumberField(null = False, blank = True, unique = True)
+    
