7feaac8fcb7f20e34c5ce590feb707cbc00c27be
yashrastogi543 <21bcs249@iiitdmj.ac.in>
2024-05-15 01:19:21 +0530

    add  views and serializer

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # ride model urls to be added below
-    # path("ride/", include(api.ride.urls)),
+    path("api/ride/", include('api.ride.urls')),
 ]
