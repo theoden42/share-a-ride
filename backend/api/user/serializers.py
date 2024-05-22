38af04b62f7034cd2e915eae15c1c1991ac459cc
Prakash <dprakash2103@gmail.com>
2024-05-23 01:13:04 +0530

    added ride views and jwt

--- backend/api/user/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Profile
-
-
-class UserSerializer(serializers.Serializer):
-    class Meta:
-        model = Profile
-        fields = '__all__'
