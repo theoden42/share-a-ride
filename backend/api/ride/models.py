65ecd8d3ab10f3f3f0612932bc884c2307be4c0a
Prakash <dprakash2103@gmail.com>
2024-06-21 03:11:01 +0530

    added state variable for ride

--- backend/api/ride/models.py
+++ backend/api/ride/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from ..user.models import Profile
 
+ride_state = (
+    ('Upcoming', 'Upcoming'),
+    ('Cancelled', 'Cancelled'),
+    ('Completed', 'Completed')
+)
+
 
 class Ride(models.Model):
+
     user_initiator = models.ForeignKey(Profile, on_delete=models.PROTECT)
     src_location = models.CharField(max_length=100)
     dst_location = models.CharField(max_length=100)
     total_fare = models.IntegerField()
     num_passengers = models.IntegerField()
     start_datetime = models.DateTimeField()
+    ride_status = models.CharField(
+        max_length=10, choices=ride_state, default="Upcoming")
