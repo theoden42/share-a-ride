b80bddc7295dfafbb245519a57530a3f971cc7c0
Prakash <dprakash2103@gmail.com>
2024-06-28 03:57:52 +0530

    adding request view

--- backend/api/ride/models.py
+++ backend/api/ride/models.py
@@ -7,6 +7,11 @@ ride_state = (
     ('Completed', 'Completed')
 )
 
+request_status = (
+    ('Accepted', 'Accepted'),
+    ('Rejected', 'Rejected'),
+    ('Pending', 'Pending')
+)
 
 class Ride(models.Model):
 
@@ -16,5 +21,12 @@ class Ride(models.Model):
     total_fare = models.IntegerField()
     num_passengers = models.IntegerField()
     start_datetime = models.DateTimeField()
+    co_passengers = models.ManyToManyField(Profile)
     ride_status = models.CharField(
         max_length=10, choices=ride_state, default="Upcoming")
+
+    
+class Passengers(models.model):
+    user = models.ForeignKey(Profile,on_delete=models.PROTECT, related_name = 'copassenger')
+    ride = models.ForeignKey(Ride, on_delete=models.PROTECT, related_name = 'copassenger')
+    request_status = models.CharField(max_length=10, choices = request_status, default='Pending' )
--- backend/api/ride/serializer.py
+++ backend/api/ride/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Ride
+from .models import Ride, Passengers
 
 
 class RideSerializer(serializers.ModelSerializer):
@@ -14,5 +14,23 @@ class RideSerializer(serializers.ModelSerializer):
             if field in validated_data:
                 setattr(instance, field, validated_data[field])
 
+        instance.save()
+        return instance
+    
+class PassengerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Passengers
+        fields = '__all__'
+        
+    def update(self, instance, validated_data):
+        
+        old_status = instance.request_status
+        new_status = validated_data['request_status']
+
+
+        if old_status != 'Accepted' and new_status == 'Accepted':
+            instance.ride.co_passengers.add(instance.user)
+            
+        setattr(instance, 'request_status', validated_data['request_status'])
         instance.save()
         return instance
